6d87c61e708140583fc49631df3c52348aba66a6
Arman Salmani <95583007+Arman1817@users.noreply.github.com>
2024-03-07 23:34:52 +0530

    Business Logic

---
+++ src/java/Contact.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.bson.Document;
+
+/**
+ *
+ * @author Arman Salmani
+ */
+public class Contact extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+String name, phone, eml, mess, time, lname;
+            name = request.getParameter("first_name");
+            lname= request.getParameter("lst_name");
+            phone = request.getParameter("phone");
+            eml = request.getParameter("email");
+            mess = request.getParameter("comments");
+            time = request.getParameter("select_service");
+
+          
+           MongoClient mongoClient = new MongoClient("localhost", 27017);
+                MongoDatabase database = mongoClient.getDatabase("Hasan_Salon");
+                MongoCollection<Document> collection = database.getCollection("Appointments");
+
+                Document doc = new Document("Name", name)
+                        .append("Phone", phone)
+                        .append("Email", eml)
+                        .append("Time", time)
+                        .append("Message", mess);
+                collection.insertOne(doc);
+
+                out.println("<h2> Appointment Booked Successfull.....</h2>"); 
+        
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
